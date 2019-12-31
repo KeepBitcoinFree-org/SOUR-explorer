0df49c4784e2f4c10236855cdff1508fdf21054d
KeepBitcoinFree-org <jigsawcrew@protonmail.com>
2019-12-31 02:02:15 -0500

    making major updates to use connection socket for each user instead of default generic socket for everyone.

--- index.js
+++ index.js
@@ -14,6 +14,70 @@ let bitbox = new BITBOX({ restURL: 'https://rest.bitcoin.com/v2/' });
 let SLPSDK = require('slp-sdk');
 let SLP = new SLPSDK();
 
+//SLPDB QUERYS - 
+
+//get all memo transactions
+const queryMemo = {
+  "v": 3,
+  "q": {
+    "find": { "out.h1": "6d02" },
+    // "out.b0": { "op": 106 },
+ //   "project": { "out.$": 1 }
+  },
+  "r": {
+    "f": "[ .[] | {msg: .out[0].s2} ]"
+  }
+}
+
+//PULLS ALL UNCONFIRMED SOUR SLP TX
+const querySOURslp = {
+    "v": 3,
+    "q": {
+  //    db: ['t'],
+      "find": {"slp.detail.tokenIdHex" : "6448381f9649ecacd8c30189cfbfee71a91b6b9738ea494fe33f8b8b51cbfca0"},
+      "limit": 50
+    },
+}
+
+//PULLS ALL UNCONFIRMED SLP TX
+const queryAllUnconf = {
+  "v": 3,
+  "q": {
+    "db": ["u"],
+    "find": { },
+    "limit": 50
+ //   "project": { "out.$": 1, "_id": 0 },
+  },
+//    "r": {
+ //   "f": "[.[] | .out[0] | { token_symbol: .s4, token_name: .s5, document_url: .s6} ]"
+ // }
+}
+// "Find 100 transactions with an output OP_RETURN script that starts with 0x6d02 (memo.cash). And then return only its block index, block time, and content."
+
+//SUPPOSED TO FIND MEMO FORMATS? IDK
+const queryMemoFormat = {
+  "v": 3,
+  "q": {
+    "find": { "out.h1": "6d02" },
+    "limit": 100
+  },
+  "r": {
+    "f": "[{ block: .blk.i?, timestamp: .blk.t?, content: .out[1].s2 }]"
+  }
+}
+
+//pull latest info for SOUR TOKEN ID 
+const querySOURtoken = {
+  "v": 3,
+  "q": {
+    "find": { "tokenDetails.tokenIdHex" : "6448381f9649ecacd8c30189cfbfee71a91b6b9738ea494fe33f8b8b51cbfca0" },
+    "limit": 10
+  }
+}
+
+
+
+
 function convertUnixTimeToTime(unixtimestamp) {
 
  // Unixtimestamp
@@ -62,7 +126,7 @@ function openSLPDBsocket(query) {
   //try to print info using socket
 }
 
-function hitSLPDBforSOURid(query) {
+function hitSLPDBforSOURid(query, socket) {
 
  (async () => {
 
@@ -75,25 +139,27 @@ function hitSLPDBforSOURid(query) {
     // console.log('res.t.tokenDetails', res.t[0].tokenDetails);
     // console.log('res.t.tokenStats', res.t[0].tokenStats);
   
-     io.emit('chat message', 'ID: ' + res.t[0].tokenDetails.tokenIdHex);
-     io.emit('chat message', 'Symbol: ' + res.t[0].tokenDetails.symbol);
-     io.emit('chat message', 'Name: ' + res.t[0].tokenDetails.name);
-     io.emit('chat message', 'Decimals: ' + res.t[0].tokenDetails.decimals);
-     io.emit('chat message', 'Version Type: ' + res.t[0].tokenDetails.versionType);
-     io.emit('chat message', 'Contains Baton: ' + res.t[0].tokenDetails.containsBaton);
-     io.emit('chat message', 'Block Created: ' + numberWithCommas(res.t[0].tokenStats.block_created));
-     io.emit('chat message', 'Timestamp: ' + res.t[0].tokenDetails.timestamp);
-     io.emit('chat message', 'Block Last Active Send: ' + numberWithCommas(res.t[0].tokenStats.block_last_active_send));
-     io.emit('chat message', 'Txns Since Genesis: ' + numberWithCommas(res.t[0].tokenStats.qty_valid_txns_since_genesis));
-     io.emit('chat message', 'Valid SOUR Addresses: ' + numberWithCommas(res.t[0].tokenStats.qty_valid_token_addresses));
-     io.emit('chat message', 'Initial Token Qty: ' + numberWithCommas(res.t[0].tokenStats.qty_token_minted) + ' SOUR');
-     io.emit('chat message', 'Total SOUR burned: ' + numberWithCommas(res.t[0].tokenStats.qty_token_burned) + ' SOUR');
-     io.emit('chat message', 'Circulating Supply: ' + numberWithCommas(res.t[0].tokenStats.qty_token_circulating_supply) + ' SOUR');
-     io.emit('chat message', 'Satoshis locked up: ' + numberWithCommas(res.t[0].tokenStats.qty_satoshis_locked_up) + ' Sats');    })();
+     socket.emit('chat message', 'ID: ' + res.t[0].tokenDetails.tokenIdHex);
+     socket.emit('chat message', 'Symbol: ' + res.t[0].tokenDetails.symbol);
+     socket.emit('chat message', 'Name: ' + res.t[0].tokenDetails.name);
+     socket.emit('chat message', 'Decimals: ' + res.t[0].tokenDetails.decimals);
+     socket.emit('chat message', 'Version Type: ' + res.t[0].tokenDetails.versionType);
+     socket.emit('chat message', 'Contains Baton: ' + res.t[0].tokenDetails.containsBaton);
+     socket.emit('chat message', 'Block Created: ' + numberWithCommas(res.t[0].tokenStats.block_created));
+     socket.emit('chat message', 'Timestamp: ' + res.t[0].tokenDetails.timestamp);
+     socket.emit('chat message', 'Block Last Active Send: ' + numberWithCommas(res.t[0].tokenStats.block_last_active_send));
+     socket.emit('chat message', 'Txns Since Genesis: ' + numberWithCommas(res.t[0].tokenStats.qty_valid_txns_since_genesis));
+     socket.emit('chat message', 'Valid SOUR Addresses: ' + numberWithCommas(res.t[0].tokenStats.qty_valid_token_addresses));
+     socket.emit('chat message', 'Initial Token Qty: ' + numberWithCommas(res.t[0].tokenStats.qty_token_minted) + ' SOUR');
+     socket.emit('chat message', 'Total SOUR burned: ' + numberWithCommas(res.t[0].tokenStats.qty_token_burned) + ' SOUR');
+     socket.emit('chat message', 'Circulating Supply: ' + numberWithCommas(res.t[0].tokenStats.qty_token_circulating_supply) + ' SOUR');
+     socket.emit('chat message', 'Satoshis locked up: ' + numberWithCommas(res.t[0].tokenStats.qty_satoshis_locked_up) + ' Sats');    
+
+   })();
 
 }
 
-function hitSLPDBforSOUR(query) {
+function hitSLPDBforSOUR(query, socket) {
 
 try {
 
@@ -129,7 +195,7 @@ try {
 // GO THROUGH UNCONFIRMED ARRAY OF TX
 //console.log(res.u);
 if (res.u.length < 1){ 
-          io.emit('tx-uo', '{ No new unconfirmed (pending) SOUR transactions avail from SLPDB. }');
+          socket.emit('tx-uo', '{ No new unconfirmed (pending) SOUR transactions avail from SLPDB. }');
     }else{
 
  //  console.log('RESPONSE : ');
@@ -154,40 +220,42 @@ if (res.u.length < 1){
    //DateTime doesn't exist in unconfirmed blocks because the timestamp is based the first block that confirms it.
 
      // io.emit('tx-c', 'Token: ' + v.slp.detail.symbol + ', ID: ' + v.slp.detail.tokenIdHex);
-    io.emit('tx-u', 'Tx Type: ' + v.slp.detail.transactionType);
+    socket.emit('tx-u', 'Tx Type: ' + v.slp.detail.transactionType);
    // io.emit('tx-c', );
 
 
-   //FOR EACH INPUTS
-
+    //FOR EACH INPUTS
     //LATEST UNCONFIRMED TRANSACTIONS OUTPUTS
     let out = 1;
     let faucet = false;
     
+    
+
+    //determine if this is the SOUR Faucet to display an img link
+    if (v.slp.detail.outputs.length > 1){
+      if (v.slp.detail.outputs[1].address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk'){
+        faucet = true;
+      }else{
+        faucet = false;
+      }
+
+    }
+    
     //FOR EACH OUTPUTS
     v.slp.detail.outputs.forEach((o) => {
-     // console.log('new output');
-    // console.log(o.address);
-      // if(o.address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk'){
-      //   io.emit('tx', 'Output #' + n + ' Addr: ' + o.address + ' (SOUR Faucet at sour-faucet.ddns.net)');
-      //   faucet = true;
-      // }else{
-     io.emit('tx-uo', 'Output #' + out + ' -');
 
-      if(o.address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk'){
-       io.emit('tx-uo', 'Addr: ' + o.address);
-       io.emit('tx-uo', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR');
-       io.emit('tx-uo', '*This is the SOUR FAUCET at http://sour-faucet.ddns.net');
+      socket.emit('tx-uo', 'Output #' + out + ' -');
 
-      }
-      else{
-        io.emit('tx-uo', 'Addr: ' + o.address);
-        io.emit('tx-uo', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR');
 
+      if((faucet == true) && (out == 1)){
+        socket.emit('tx-co-f', ' This is the SOUR Faucet');
       }
 
-     out++;
-    });
+      io.emit('tx-uo', 'Output #' + n + ' -');
+        socket.emit('tx-uo', 'Addr: ' + o.address);
+        socket.emit('tx-uo', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR');
+        out++;
+      });
 
 
     });
@@ -202,7 +270,7 @@ if (res.u.length < 1){
 
    //check if it's empty (shouldn't be though)
    if (res.c.length < 1){ 
-          io.emit('tx-c', '{ No new confirmed (completed) SOUR transactions avail from SLPDB. }');
+          socket.emit('tx-c', '{ No new confirmed (completed) SOUR transactions avail from SLPDB. }');
     }else{
 
  //  console.log('RESPONSE : ');
@@ -224,43 +292,39 @@ if (res.u.length < 1){
    let utcDate = convertUnixTimeToTime(timestamp);
   //  console.log('utcDate', utcDate);
    
-    io.emit('tx-c', 'Confirmed on: ' + utcDate + ' UTC,  Tx Type: ' + v.slp.detail.transactionType);
+    socket.emit('tx-c', 'Confirmed on: ' + utcDate + ' UTC,  Tx Type: ' + v.slp.detail.transactionType);
 
     //LATEST UNCONFIRMED TRANSACTIONS
     let n = 1;
     let faucet = false;
     
-    v.slp.detail.outputs.forEach((o) => {
-     // console.log('new output');
-    // console.log(o.address);
-      // if(o.address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk'){
-      //   io.emit('tx', 'Output #' + n + ' Addr: ' + o.address + ' (SOUR Faucet at sour-faucet.ddns.net)');
-      //   faucet = true;
-      // }else{
-     io.emit('tx-co', 'Output #' + n + ' -');
-     if(o.address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk'){
-        io.emit('tx-co', 'Addr: ' + o.address);
-        io.emit('tx-co', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR');
-        io.emit('tx-co', '*This is the SOUR FAUCET at http://sour-faucet.ddns.net');
-      }
-      else{
-
-        io.emit('tx-co', 'Addr: ' + o.address);
-        io.emit('tx-co', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR');
-      }
-// ', Amount: ' + numberWithCommasPeriod(o.amount) + ' SOUR'
-   //   if ((n == 1) && (v.slp.detail.outputs[1].address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk')){
-   //   io.emit('tx', 'Output #' + n + ' Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR (WON rolling the dice at the SOUR Faucet');
-
-  //    }else{
     
-  //   }
-     n++;
-    });
 
-    //io.emit('tx', '=================================================================');
-  //  io.emit('update', 'Details: ' + JSON.stringify(v));
-   // io.emit('update', '');
+    //determine if this is the SOUR Faucet to display an img link
+    if (v.slp.detail.outputs.length > 1){
+      if (v.slp.detail.outputs[1].address == 'simpleledger:qqs74sypnfjzkxeq0ltqnt76v5za02amfgy9zcr9mk'){
+        faucet = true;
+      }else{
+        faucet = false;
+      }
+
+    }
+
+    //FOR EACH - CONFIRMED OUTPUTS
+    v.slp.detail.outputs.forEach((o) => {
+
+        if((faucet == true) && (n == 1)){
+          socket.emit('tx-co-f', ' This is the SOUR Faucet');
+        }
+
+        socket.emit('tx-co', 'Output #' + n + ' -');
+        socket.emit('tx-co', 'Addr: ' + o.address);
+        socket.emit('tx-co', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR');
+        if((faucet == true) && (n == 1)){
+          socket.emit('tx-co', 'Amnt: ' + numberWithCommasPeriod(o.amount) + ' SOUR (WON at the SOUR Faucet)');
+        }
+        n++;
+    });
     });
     //io.emit('break', '.');
 
@@ -270,15 +334,6 @@ if (res.u.length < 1){
 
 })();
 
-  // io.emit('update', 'Latest SOUR Addresses: ');
-  // io.emit('update', JSON.stringify(res.a[0], null, 4));
-
-
-  // io.emit('update', 'SOUR ADDRESSES: ');
-  // res.a.forEach((v) => {
-  //   io.emit('update', v.address);
-  //   io.emit('update', v.token_balance + ' SOUR');
-  //});
 
 }catch(error){
 console.log(error);
@@ -287,56 +342,18 @@ console.log(error);
  //end fucntion
 }
 
+
+
+
+
+
+
+
+
+// USER HAS REQUESTED INDEX / PAGE
 app.get('/', function(req, res){
- 
-
-//io.emit('chat message', 'SOUR Tracker');
- 
-
-//console.log('hitting SLPDB for SOURid');
-//hitSLPDBforSOURid(querySOURtoken);
-
-
- try {
- // (async () => {
-    
- //       let sour = await SLP.Utils.list(
- //         '6448381f9649ecacd8c30189cfbfee71a91b6b9738ea494fe33f8b8b51cbfca0'
- //       );
-
- //     // console.log(sour);
- //     // console.log('whatup');
-
- //     io.emit('chat message', 'ID: ' + sour.id);
- //     io.emit('chat message', 'Symbol: ' + sour.symbol);
- //     io.emit('chat message', 'Name: ' + sour.name);
- //     io.emit('chat message', 'Decimals: ' + sour.decimals);
- //     io.emit('chat message', 'Timestamp: ' + sour.timestamp);
- //     io.emit('chat message', 'Version Type: ' + sour.versionType);
- //     io.emit('chat message', 'Contains Baton: ' + sour.containsBaton);
- //     //io.emit('chat message', 'Doc Hash: ' + sour.documentHash);
- //     io.emit('chat message', 'Block Created: ' + numberWithCommas(sour.blockCreated));
- //     io.emit('chat message', 'Block Last Active Send: ' + numberWithCommas(sour.blockLastActiveSend));
-	//    io.emit('chat message', 'Txns Since Genesis: ' + numberWithCommas(sour.txnsSinceGenesis));
-	//    io.emit('chat message', 'Valid SOUR Addresses: ' + numberWithCommas(sour.validAddresses));
-	//    io.emit('chat message', 'Initial Token Qty: ' + numberWithCommas(sour.initialTokenQty) + ' SOUR');
-	//    io.emit('chat message', 'Total SOUR burned: ' + numberWithCommas(sour.totalBurned));
-	//    io.emit('chat message', 'Circulating Supply: ' + numberWithCommas(sour.circulatingSupply) + ' SOUR');
- //   //  io.emit('chat message', '_________________________________________________________________________________');
-
-
-//     io.emit('logo', '__┬__┌─┐┌┬┐┌─┐┌─┐┌┬┐__╔═╗╔═╗╦_╦╦═╗__┌┬┐┬─┐┌─┐┌┐┌┌─┐┌─┐┌─┐┌┬┐┬┌─┐┌┐┌┌─┐');
-//     io.emit('logo', '__│__├─┤_│_├┤_└─┐_│___╚═╗║_║║_║╠╦╝___│_├┬┘├─┤│││└─┐├─┤│___│_││_││││└─┐');
-//     io.emit('logo', '__┴─┘┴_┴_┴_└─┘└─┘_┴___╚═╝╚═╝╚═╝╩╚═___┴_┴└─┴_┴┘└┘└─┘┴_┴└─┘_┴_┴└─┘┘└┘└─┘');
- //    io.emit('update', '__██████__▒█████___█____██__██▀███_____▄▄▄█████▓▒██___██▒');
- //    io.emit('update', '▒██____▒_▒██▒__██▒_██__▓██▒▓██_▒_██▒___▓__██▒_▓▒▒▒_█_█_▒░');
- //    io.emit('update', '░_▓██▄___▒██░__██▒▓██__▒██░▓██_░▄█_▒___▒_▓██░_▒░░░__█___░');
- //    io.emit('update', '__▒___██▒▒██___██░▓▓█__░██░▒██▀▀█▄_____░_▓██▓_░__░_█_█_▒_');
- //    io.emit('update', '▒██████▒▒░_████▓▒░▒▒█████▓_░██▓_▒██▒_____▒██▒_░_▒██▒_▒██▒');
- //    io.emit('update', '▒_▒▓▒_▒_░░_▒░▒░▒░_░▒▓▒_▒_▒_░_▒▓_░▒▓░_____▒_░░___▒▒_░_░▓_░');
- //    io.emit('update', '░_░▒__░_░__░_▒_▒░_░░▒░_░_░___░▒_░_▒░_______░____░░___░▒_░');
- //    io.emit('update', '░__░__░__░_░_░_▒___░░░_░_░___░░___░______░_______░____░__');
-  
+  //Actually returning the index html page
+  res.sendFile(__dirname + '/index.html');
 
   // awesome documentation
   // https://docs.fountainhead.cash/docs/tutorial_v3
@@ -344,88 +361,25 @@ app.get('/', function(req, res){
   // https://docs.fountainhead.cash/docs/intro_v3#query-examples
   // https://docs.fountainhead.cash/docs/query_v3
   // https://github.com/simpleledger/SLPDB/blob/master/README.md#BCHDgRPCSupport
+ 
+});
 
 
 
- // })();
+ // NEW USER CONNECTED HERE
+io.on('connection', function(socket){
 
-    } catch (error) {
-      console.log(error);
-    }
-
- try{ 
-        // GET REAL-TIME DATA FROM SOCKET
-        // open REAL TIME Socket
-
+  try{ 
 
   //QUERIES FOR SLPDB TO GRAB INFO ON SOUR
 
-  //MEMO QUERY - get all memo transactions
-var queryMemo = {
-  "v": 3,
-  "q": {
-    "find": { "out.h1": "6d02" },
-    // "out.b0": { "op": 106 },
- //   "project": { "out.$": 1 }
-  },
-  "r": {
-    "f": "[ .[] | {msg: .out[0].s2} ]"
-  }
-}
-
-var querySOURslp = {
-    "v": 3,
-    "q": {
-  //    db: ['t'],
-      "find": {"slp.detail.tokenIdHex" : "6448381f9649ecacd8c30189cfbfee71a91b6b9738ea494fe33f8b8b51cbfca0"},
-      "limit": 50
-    },
-}
-
-var queryAllUnconf = {
-  "v": 3,
-  "q": {
-    "db": ["u"],
-    "find": { },
-    "limit": 50
- //   "project": { "out.$": 1, "_id": 0 },
-  },
-//    "r": {
- //   "f": "[.[] | .out[0] | { token_symbol: .s4, token_name: .s5, document_url: .s6} ]"
- // }
-}
-
-// "Find 100 transactions with an output OP_RETURN script that starts with 0x6d02 (memo.cash). And then return only its block index, block time, and content."
-
-var queryMemoFormat = {
-  "v": 3,
-  "q": {
-    "find": { "out.h1": "6d02" },
-    "limit": 100
-  },
-  "r": {
-    "f": "[{ block: .blk.i?, timestamp: .blk.t?, content: .out[1].s2 }]"
-  }
-}
-
-var querySOURtoken = {
-  "v": 3,
-  "q": {
-    "find": { "tokenDetails.tokenIdHex" : "6448381f9649ecacd8c30189cfbfee71a91b6b9738ea494fe33f8b8b51cbfca0" },
-    "limit": 10
-  }
-}
+  //console.log(querySOURtoken);
+  hitSLPDBforSOURid(querySOURtoken, socket);
 
 
-//console.log(querySOURtoken);
-  hitSLPDBforSOURid(querySOURtoken);
-//console.log('hitSLPDBforSOURid');
+  //console.log('hitSLPDBforSOURid');
+  hitSLPDBforSOUR(querySOURslp, socket);
 
-
-
-//setTimeout(
-
-  hitSLPDBforSOUR(querySOURslp);
 
   //, 10 * 1000);
 
@@ -456,13 +410,13 @@ var querySOURtoken = {
 
 // https://slpsocket.bchdata.cash/
 
- res.sendFile(__dirname + '/index.html');
+  //CLOSE USER SOCKET CONNECTION
+  });
 
 
 
 
 
-});
 
 //  io.on('connection', function(socket){
 //    console.log('new user connected to socket');
